c94020f806310e38c09e49e331fdee8d5a8e4c61
jooman21 <yohannesgezahugne59@gmail.com>
2022-06-07 11:49:29 +0300

    joo21

--- alx-higher_level_programming/11-delete_at.py
+++ alx-higher_level_programming/11-delete_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    n = len(my_list)
-    new_list = []
-    if idx < 0 or idx > n - 1:
-        return my_list
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
     else:
-        for i in range(n):
-            if i is idx:
-                del my_list[i]
-                return my_list
+        del my_list[idx]
+        return (my_list)
